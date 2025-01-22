a700fc178756275fbc3535d23117545e7abab5a9
grindghost <grindghost@mail.com>
2025-01-22 13:35:09 -0500

    Removed wrong statement import

--- pages/units/embed.vue
+++ pages/units/embed.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-  import UnitEmbed from '~/components/UnitEmbed.vue';
-
-  // Disable layout
-  definePageMeta({
-    layout: false,
-  });
 
 </script>
 
 <template>
-  <UnitEmbed  />
   <div>embed</div>
 </template>
